7f28abeb322124bab1089fa083c029381555a2c8
ztx318 <1159647587@qq.com>
2017-06-23 01:37:46 +0800

    不不不

--- app/src/main/java/com/example/shanson/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/shanson/myapplication/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.shanson.myapplication.activity;
 
 
-import android.content.DialogInterface;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -9,6 +8,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -16,12 +16,14 @@ import android.widget.NumberPicker;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.shanson.myapplication.base.BaseActivity;
 import com.example.shanson.myapplication.base.adapter.MyAdapter;
 import com.example.shanson.myapplication.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 
@@ -34,6 +36,7 @@ public class MainActivity extends BaseActivity {
     private RelativeLayout wp_type;
     private RelativeLayout wp_weight;
     private PopupWindow popupWindow;
+    private PopupWindow popupDatePicWindow;
     private Button btn_cancle;
     private Button btn_command;
     private TextView number_price;
@@ -46,6 +49,18 @@ public class MainActivity extends BaseActivity {
     private EditText phonenumber;
     private Button sendpwd;
     private LinearLayout custom_dialog;
+    private RelativeLayout wp_gettime;
+    private DatePicker date_picker;
+    private TimePicker time_picker;
+    private TimePicker time_picker2;
+    private TextView gettime_wp;
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+    private Button btn_command2;
+    private Button btn_cancle2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,12 +76,16 @@ public class MainActivity extends BaseActivity {
         wp_weight = (RelativeLayout) findViewById(R.id.wp_weight);
         text_weight = (TextView) findViewById(R.id.text_weight);
         text_privi = (TextView) findViewById(R.id.text_privilege);
+        wp_gettime = (RelativeLayout) findViewById(R.id.wp_gettime);
+        gettime_wp = (TextView) findViewById(R.id.gettime_wp);
 
+        gettime_wp.setOnClickListener(new MyListener());
         text_privi.setOnClickListener(new MyListener());
         wp_weight.setOnClickListener(new MyListener());
         wp_go.setOnClickListener(new MyListener());
         wp_come.setOnClickListener(new MyListener());
         wp_type.setOnClickListener(new MyListener());
+        wp_gettime.setOnClickListener(new MyListener());
     }
 
     private View.OnClickListener clickListener =
@@ -104,11 +123,6 @@ public class MainActivity extends BaseActivity {
                 case R.id.btn_command:
                     popupWindow.dismiss();
                     text_weight.setText("" + weight + "公斤");
-                case R.id.wp_time:
-                initPopWindow();
-
-
-
                     break;
                 case R.id.text_privilege:
                     initDialog();
@@ -116,10 +130,20 @@ public class MainActivity extends BaseActivity {
                 case R.id.text_cancle:
                     dialog.dismiss();
                     break;
+                case R.id.wp_gettime:
+                    initDateWindow();
+                    break;
                 case R.id.send_check:
                     // TODO: 2017/6/21
                     sendpwd.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_dark));
 
+                    break;
+                case R.id.btn_command2:
+                    showDate();
+                    break;
+                case R.id.btn_cancle2:
+                    popupDatePicWindow.dismiss();
+                    gettime_wp.setText("立即取件");
                     break;
 
             }
@@ -128,25 +152,55 @@ public class MainActivity extends BaseActivity {
 
     }
 
-    private void initPopWindow() {
+    private void initDateWindow() {
+        //设置contentView
+        View contentView = LayoutInflater.from(MainActivity.this).inflate(R.layout.popdate_picker, null);
+        popupDatePicWindow = new PopupWindow(contentView,
+                ViewPager.LayoutParams.MATCH_PARENT, ViewPager.LayoutParams.WRAP_CONTENT);
 
+        popupDatePicWindow.setContentView(contentView);
+        popupDatePicWindow.setFocusable(true); // 设置PopupWindow可获得焦点
+        popupDatePicWindow.setTouchable(true); // 设置PopupWindow可触摸
+
+        date_picker = contentView.findViewById(R.id.date_picker);
+        time_picker = contentView.findViewById(R.id.time_picker);
+        btn_command2 = contentView.findViewById(R.id.btn_command2);
+        btn_cancle2 = contentView.findViewById(R.id.btn_cancle2);
+
+        btn_command2.setOnClickListener(new MyListener());
+        btn_cancle2.setOnClickListener(new MyListener());
+
+        Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+        year = c.get(Calendar.YEAR);
+
+        //初始化组件
+        date_picker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                MainActivity.this.year = year;
+                MainActivity.this.month = month;
+                MainActivity.this.day = day;
+            }
+        });
+
+        time_picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                MainActivity.this.hour=hour;
+                MainActivity.this.minute=minute;
+            }
+        });
 
     }
 
-    private void initDialog() {
-        custom_dialog = (LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_dialog, null);
-        dialog = new AlertDialog.Builder(this).setView(custom_dialog).create();
-        dialog.show();
-        cancle = custom_dialog.findViewById(R.id.text_cancle);
-        phonenumber = custom_dialog.findViewById(R.id.edit_phonenub);
-        sendpwd = custom_dialog.findViewById(R.id.send_check);
-        sendpwd.setOnClickListener(new MyListener());
-        phonenumber.setOnClickListener(new MyListener());
-        cancle.setOnClickListener(new MyListener());
-
+    //
+    private void showDate() {
+        gettime_wp.setText(""+month+"月"+day+"号"+hour+"时"+minute+"分");
 
     }
-
 
     private void initPopupWindow() {
         //设置contentView
@@ -187,6 +241,19 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    private void initDialog() {
+        custom_dialog = (LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_dialog, null);
+        dialog = new AlertDialog.Builder(this).setView(custom_dialog).create();
+        dialog.show();
+        cancle = custom_dialog.findViewById(R.id.text_cancle);
+        phonenumber = custom_dialog.findViewById(R.id.edit_phonenub);
+        sendpwd = custom_dialog.findViewById(R.id.send_check);
+        sendpwd.setOnClickListener(new MyListener());
+        phonenumber.setOnClickListener(new MyListener());
+        cancle.setOnClickListener(new MyListener());
+
+
+    }
 
     private void initPagerData() {
         viewPager = (ViewPager) findViewById(R.id.viewpager);
